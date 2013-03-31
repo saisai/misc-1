b476acdb4feb3774555df7a97d7f5afdbfb92126
Ilan Schnell <ilanschnell@gmail.com>
2013-03-31 01:33:06 -0500

    cleanup

--- sudoku/euler.py
+++ sudoku/euler.py
@@ -1,33 +1,35 @@
 from copy import deepcopy
+
+
 Result=0
 
-def solve( S ):
-    def options(i,j):
+def solve(S):
+    def options(i, j):
         r=list(S[i])
-        r.extend( S[k][j] for k in range(9) )
-        r.extend( S[i/3*3+k][j/3*3+m] \
-                  for k in range(3) for m in range(3) )
-        s=set(r)
+        r.extend(S[k][j] for k in range(9))
+        r.extend(S[i/3*3 + k][j/3*3 + m]
+                 for k in range(3) for m in range(3))
+        s = set(r)
         return [z for z in range(1,10) if z not in list(s)]
     mnp = 99
     mp = None
     for i in range(9):
         for j in range(9):
-            if S[i][j]==0:
-                p=options(i,j)
-                if len(p)<mnp:
+            if S[i][j] == 0:
+                p = options(i, j)
+                if len(p) < mnp:
                     mnp = len(p)
                     mp = i, j, p
     if mp == None:
         global Result
-        Result += 100*S[0][0]+10*S[0][1]+S[0][2]
+        Result += 100 * S[0][0] + 10 * S[0][1] + S[0][2]
         return
-    T=deepcopy(S)
+    T = deepcopy(S)
     i, j, p = mp
-    W=[]
+    W = []
     for c in p:
-        T[i][j]=c
-        W.append( solve( T ) )
+        T[i][j] = c
+        W.append(solve(T))
     for c in W:
         return c
 
@@ -37,7 +39,7 @@ for grid in range(50):
         raise 'Error while reading.'
     M=[]
     for j in range(9):
-        M.append([ int(c) for c in fi.readline().strip() ])
-    solve( M )
+        M.append([int(c) for c in fi.readline().strip()])
+    solve(M)
 fi.close()
 print Result
