1ab9586927cf02ee29d6109f51a68aa3480dbc70
Ilan Schnell <ilanschnell@gmail.com>
2013-04-01 01:17:53 -0500

    add test file

---
+++ sat/t.py
@@ -0,0 +1,19 @@
+import sys
+from pprint import pprint
+
+from sudoku import sudoku_clauses
+
+import pycosat
+
+
+def write_cnf(clauses, path):
+    n_vars = max(max(abs(lit) for lit in clause)
+                 for clause in clauses)
+    with open(path, 'w') as fo:
+        fo.write('p cnf %d %d\n' % (n_vars, len(clauses)))
+        for clause in clauses:
+            fo.write(' '.join(str(lit) for lit in clause))
+            fo.write(' 0\n')
+
+
+write_cnf(sudoku_clauses(), 'sudoku.cnf')
