d643b0c7ad4a2109d36d554ec7e8bda6d4c5452c
Ilan Schnell <ilanschnell@gmail.com>
2013-04-13 00:01:34 -0500

    add "packages" which have no requirements

--- sat/opium.py
+++ sat/opium.py
@@ -2,5 +2,10 @@ index = {
     'a', {'requires': ['a', 'b', 'z']},
     'b', {'requires': ['d']},
     'c', {'requires': ['d|e', 'f|g']},
+    'd', {'requires': []},
+    'e', {'requires': []},
+    'f', {'requires': []},
+    'g', {'requires': []},
     'y', {'requires': ['z']},
+    'z', {'requires': []},
 }
