ca0f89d06f2e2e9963dc42178a807f4758dd922e
Ilan Schnell <ilanschnell@gmail.com>
2013-04-19 08:26:44 -0500

    update experimental work

--- sat/i2.py
+++ sat/i2.py
@@ -33,56 +33,59 @@ def split_requirement(s):
     assert len(parts) == 3
     return tuple(parts)
 
-def reqs_pkg(fn):
-    "given a package, return all possible requirements"
-    for r in index[fn]['requires']:
-        name, version, build = split_requirement(r)
-        if version is None:
-            raise
-            assert build is None
-            for fn2, unused_info in itergroup(name):
-                yield name, version, fn2
+def find_matches(name, version, build):
+    assert name is not None
+    if version is None:
+        assert build is None
+        for fn2, unused_info in itergroup(name):
+            yield fn2
 
-        elif name in ('python', 'numpy') and len(version) == 3:
-            assert build is None
-            for fn2, info2 in itergroup(name):
-                if info2['version'].startswith(version):
-                    yield name, version, fn2
+    elif name in ('python', 'numpy') and len(version) == 3:
+        assert build is None
+        for fn2, info2 in itergroup(name):
+            if info2['version'].startswith(version):
+                yield fn2
 
-        elif build is None:
-            for fn2, info2 in itergroup(name):
-                if info2['version'] == version:
-                    yield name, version, fn2
+    elif build is None:
+        for fn2, info2 in itergroup(name):
+            if info2['version'] == version:
+                yield fn2
 
-        else:
-            fn2 = '%s-%s-%s.tar.bz2' % (name, version, build)
-            assert fn2 in index
-            yield name, version, fn2
+    else:
+        fn2 = '%s-%s-%s.tar.bz2' % (name, version, build)
+        assert fn2 in index
+        yield fn2
+
+def nvb_fn(fn):
+    return tuple(fn[:-8].rsplit('-', 2))
 
 def resolve(root_fn):
     req_names = set()
     pkgs = set()
 
     def add_dependents(fn1):
-        for name, version, fn2 in reqs_pkg(fn1):
+        for r in index[fn1]['requires']:
+            name, version, build = split_requirement(r)
             if name in req_names:
                 continue
+            for fn2 in find_matches(name, version, build):
+                pkgs.add(fn2)
+                add_dependents(fn2)
             req_names.add(name)
-            for f in files:
-                pkgs.add(f)
-                add_dependents(f)
 
     add_dependents(root_fn)
     return pkgs
 
-if 1:
-    fn = 'anaconda-1.4.1-np17py27_0.tar.bz2'
-    #pprint(index[fn])
-    ana_shallow = set()
-    for name, version, fn in reqs_pkg(fn):
-        ana_shallow.add(fn)
-    for f1 in ana_shallow:
-        print 'ana_shallow ========', f1
-        for f in resolve(f1):
-            print f
-            #assert f in ana_shallow, f
+fn = 'anaconda-1.4.1-np17py27_0.tar.bz2'
+#fn = 'bitarray-0.8.0-py26_0.tar.bz2'
+#
+pprint(resolve(fn))
+#ana_shallow = set()
+#for name, version, fn in reqs_pkg(fn):
+#    ana_shallow.add(fn)
+#for f1 in ana_shallow:
+#    print 'ana_shallow ========', f1
+#    for f in resolve(f1):#
+#        print f
+#        #assert f in ana_shallow, f
+#print list(find_matches(*nvb_fn(fn)))
