6accbda5f0f61669651772b57656e01c4f5c0194
Ilan Schnell <ilanschnell@gmail.com>
2013-04-30 19:16:26 -0500

    update experimental work

--- sat/resolve.py
+++ sat/resolve.py
@@ -8,33 +8,27 @@ from install import index, split_requirement, find_matches
 def nvb_fn(fn):
     return tuple(fn[:-8].rsplit('-', 2))
 
-def resolve(root_fn):
-    req_names = set()
+def shallow_deps(fn):
+    pkgs = set()
+    for r in index[fn]['requires']:
+        for fn2 in find_matches(*split_requirement(r)):
+            pkgs.add(fn2)
+    return pkgs
+
+def all_deps(root_fn):
     pkgs = set()
 
     def add_dependents(fn1):
         for r in index[fn1]['requires']:
-            name, version, build = split_requirement(r)
-            if name in req_names:
-                continue
-            for fn2 in find_matches(name, version, build):
+            for fn2 in find_matches(*split_requirement(r)):
                 pkgs.add(fn2)
                 add_dependents(fn2)
-            req_names.add(name)
 
     add_dependents(root_fn)
     return pkgs
 
 fn = 'anaconda-1.4.1-np17py27_0.tar.bz2'
-#fn = 'bitarray-0.8.0-py26_0.tar.bz2'
-#
-pprint(resolve(fn))
-#ana_shallow = set()
-#for name, version, fn in reqs_pkg(fn):
-#    ana_shallow.add(fn)
-#for f1 in ana_shallow:
-#    print 'ana_shallow ========', f1
-#    for f in resolve(f1):#
-#        print f
-#        #assert f in ana_shallow, f
-#print list(find_matches(*nvb_fn(fn)))
+
+a = all_deps(fn)
+s = shallow_deps(fn)
+assert s - a == set([])
