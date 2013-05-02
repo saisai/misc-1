9e6bbf60f5c56a8d44c1f1ac50e60b0e8548bbc7
Ilan Schnell <ilanschnell@gmail.com>
2013-05-02 18:01:32 -0500

    update index data

--- conda/mk_index.py
+++ conda/mk_index.py
@@ -7,5 +7,15 @@ index = get_index()
 for info in index.itervalues():
     del info['channel'], info['md5'], info['size']
 
+
+for fn in index.keys():
+    if fn.startswith(('accelerate-', 'mkl-devel')):
+        del index[fn]
+
+
+data = json.dumps(index, indent=2, sort_keys=True)
+data = '\n'.join(line.rstrip() for line in data.split('\n'))
+if not data.endswith('\n'):
+    data += '\n'
 with open('index.json', 'w') as fo:
-    json.dump(index, fo, indent=2, sort_keys=True)
+    fo.write(data)
