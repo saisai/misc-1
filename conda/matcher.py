6133986aee4d0bc7ad2c3b655b8f458152a2dbc7
Ilan Schnell <ilanschnell@gmail.com>
2013-05-03 20:39:05 -0500

    simplifications

--- conda/matcher.py
+++ conda/matcher.py
@@ -7,27 +7,24 @@ class MatchSpec(object):
         self.strictness = len(parts)
         assert 1 <= self.strictness <= 3
         self.name = parts[0]
-        if self.strictness >= 2:
+
+        if self.strictness == 2:
             rx = parts[1]
             rx = rx.replace('.', r'\.')
             rx = rx.replace('*', r'.*')
             rx = r'(%s)$' % rx
             self.ver_pat = re.compile(rx)
-        else:
-            self.ver_pat = None
 
-        if self.strictness == 3:
-            self.build = parts[2]
-        else:
-            self.build = None
+        elif self.strictness == 3:
+            self.ver_build = tuple(parts[1:3])
 
     def match(self, dist):
         name, version, build = dist.rsplit('-', 2)
         if name != self.name:
             return False
-        if self.ver_pat and not self.ver_pat.match(version):
+        if self.strictness == 2 and self.ver_pat.match(version) is None:
             return False
-        if self.build and build != self.build:
+        if self.strictness == 3 and ((version, build) != self.ver_build):
             return False
         return True
 
@@ -43,7 +40,6 @@ if __name__ == '__main__':
                        ('numpy 1.6.2|1.7.1', True),
                        ('numpy 1.6.2|1.7.0', False),
                        ('numpy 1.7.1 py27_0', True),
-                       ('numpy 1.7.* py27_0', True),
                        ('numpy 1.7.1 py26_0', False),
                        ('python', False),
                        ]:
