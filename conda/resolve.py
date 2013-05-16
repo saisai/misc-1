d00a4cba081e21597571ddbd82c19b745cd09e7d
Ilan Schnell <ilanschnell@gmail.com>
2013-05-15 19:47:54 -0500

    add solve()

--- conda/resolve.py
+++ conda/resolve.py
@@ -1,8 +1,11 @@
-import re
+from pprint import pprint
+from collections import defaultdict
+
+import pycosat
 
 import verlib
 
-from install import index, groups, itergroup, find_matches
+from install import index, find_matches
 
 
 class Package(object):
@@ -61,6 +64,48 @@ def all_deps(root_fn):
     return sorted(res)
 
 
+def solve(root_fn):
+    dists = all_deps(root_fn)
+    dists.append(root_fn)
+    v = {} # map fn to variable number
+    w = {} # map variable number to fn
+    for i, fn in enumerate(dists):
+        v[fn] = i + 1
+        w[i + 1] = fn
+
+    groups = defaultdict(list) # map name to list of filenames
+    for fn in dists:
+        groups[index[fn]['name']].append(fn)
+
+    clauses = []
+
+    for filenames in groups.itervalues():
+        # ensure packages with the same name conflict
+        for fn1 in filenames:
+            v1 = v[fn1]
+            for fn2 in filenames:
+                v2 = v[fn2]
+                if v1 < v2:
+                    clauses.append([-v1, -v2])
+
+    for fn1 in dists:
+        info1 = index[fn1]
+        for ms in info1['ms_depends']:
+            clause = [-v[fn1]]
+            for fn2 in find_matches(ms):
+                if fn2 in dists:
+                    clause.append(v[fn2])
+
+            assert len(clause) > 1, fn1
+            clauses.append(clause)
+
+    clauses.append([v[root_fn]])
+    pprint(clauses)
+    for sol in pycosat.itersolve(clauses):
+        print sol
+        yield [w[lit] for lit in sol if lit > 0]
+
+
 if __name__ == '__main__':
-    for fn in all_deps('scipy-0.12.0-np15py26_0.tar.bz2'):
-        print fn
+    for lst in solve('scipy-0.12.0-np15py26_0.tar.bz2'):
+        pprint(lst)
