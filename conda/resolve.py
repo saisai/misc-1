3a12440c3f2234fd56759556badbf2a8bfb22581
Ilan Schnell <ilanschnell@gmail.com>
2013-05-19 22:06:19 -0500

    remove install.py dependency

--- conda/resolve.py
+++ conda/resolve.py
@@ -8,15 +8,13 @@ import pycosat
 
 import verlib
 from utils import iter_pairs, memoized, memoize
-from install import index, find_matches
 from matcher import MatchSpec
 
 
 class Package(object):
 
-    def __init__(self, fn):
+    def __init__(self, fn, info):
         self.fn = fn
-        info = index[fn]
         self.name = info['name']
         self.version = info['version']
         self.build_number = info['build_number']
@@ -49,184 +47,193 @@ class Resolve(object):
         self.groups = defaultdict(list) # map name to list of filenames
         for fn, info in index.iteritems():
             self.groups[info['name']].append(fn)
+        self.msd = {}
 
     def find_matches(self, ms):
         for fn in self.groups[ms.name]:
             if ms.match(fn):
                 yield fn
 
-    @memoize
     def ms_depends(self, fn):
-        return [MatchSpec(d) for d in self.index[fn]['depends']]
+        try:
+            res = self.msd[fn]
+        except KeyError:
+            res = self.msd[fn] = [MatchSpec(d)
+                                  for d in self.index[fn]['depends']]
+        return res
 
-@memoized
-def get_pkgs(ms):
-    #print ms, isinstance(ms, collections.Hashable)
-    return [Package(fn) for fn in find_matches(ms)]
+    @memoize
+    def features(self, fn):
+        return set(self.index[fn].get('features', '').split())
 
-def get_max_dists(ms):
-    pkgs = get_pkgs(ms)
-    assert pkgs
-    maxpkg = max(pkgs)
-    for pkg in pkgs:
-        if pkg == maxpkg:
-            yield pkg.fn
+    @memoize
+    def get_pkgs(self, ms):
+        #print ms, isinstance(ms, collections.Hashable)
+        return [Package(fn, self.index[fn]) for fn in self.find_matches(ms)]
 
-def all_deps(root_fn):
-    res = set()
+    def get_max_dists(self, ms):
+        pkgs = self.get_pkgs(ms)
+        assert pkgs
+        maxpkg = max(pkgs)
+        for pkg in pkgs:
+            if pkg == maxpkg:
+                yield pkg.fn
 
-    def add_dependents(fn1):
-        for ms in index[fn1]['ms_depends']:
-            for fn2 in get_max_dists(ms):
-                if fn2 in res:
-                    continue
-                res.add(fn2)
-                if ms.strictness < 3:
-                    add_dependents(fn2)
+    def all_deps(self, root_fn):
+        res = set()
 
-    add_dependents(root_fn)
-    return res
+        def add_dependents(fn1):
+            for ms in self.ms_depends(fn1):
+                for fn2 in self.get_max_dists(ms):
+                    if fn2 in res:
+                        continue
+                    res.add(fn2)
+                    if ms.strictness < 3:
+                        add_dependents(fn2)
 
-def solve2(root_dists, features, verbose=False):
-    dists = set()
-    for root_fn in root_dists:
-        dists.update(all_deps(root_fn))
-        dists.add(root_fn)
+        add_dependents(root_fn)
+        return res
 
-    groups = defaultdict(list) # map name to list of filenames
-    for fn in dists:
-        groups[index[fn]['name']].append(fn)
+    def solve2(self, root_dists, features, verbose=False):
+        dists = set()
+        for root_fn in root_dists:
+            dists.update(self.all_deps(root_fn))
+            dists.add(root_fn)
 
-    if len(groups) == len(dists):
-        assert all(len(filenames) == 1 for filenames in groups.itervalues())
-        if verbose:
-            print "No duplicate name, no SAT needed."
-        return sorted(dists)
+        groups = defaultdict(list) # map name to list of filenames
+        for fn in dists:
+            groups[self.index[fn]['name']].append(fn)
 
-    v = {} # map fn to variable number
-    w = {} # map variable number to fn
-    for i, fn in enumerate(sorted(dists)):
-        v[fn] = i + 1
-        w[i + 1] = fn
+        if len(groups) == len(dists):
+            assert all(len(filenames) == 1 for filenames in groups.itervalues())
+            if verbose:
+                print "No duplicate name, no SAT needed."
+            return sorted(dists)
 
-    clauses = []
+        v = {} # map fn to variable number
+        w = {} # map variable number to fn
+        for i, fn in enumerate(sorted(dists)):
+            v[fn] = i + 1
+            w[i + 1] = fn
 
-    for filenames in groups.itervalues():
-        # ensure packages with the same name conflict
-        for fn1 in filenames:
-            v1 = v[fn1]
-            for fn2 in filenames:
-                v2 = v[fn2]
-                if v1 < v2:
-                    clauses.append([-v1, -v2])
+        clauses = []
 
-    for fn1 in dists:
-        info1 = index[fn1]
-        for ms in info1['ms_depends']:
-            clause = [-v[fn1]]
-            for fn2 in find_matches(ms):
-                if fn2 in dists:
-                    clause.append(v[fn2])
+        for filenames in groups.itervalues():
+            # ensure packages with the same name conflict
+            for fn1 in filenames:
+                v1 = v[fn1]
+                for fn2 in filenames:
+                    v2 = v[fn2]
+                    if v1 < v2:
+                        clauses.append([-v1, -v2])
 
-            assert len(clause) > 1, fn1
-            clauses.append(clause)
-
-    for root_fn in root_dists:
-        clauses.append([v[root_fn]])
-
-    candidates = defaultdict(list)
-    for sol in pycosat.itersolve(clauses):
-        pkgs = [w[lit] for lit in sol if lit > 0]
-        fsd = sum(len(features ^ index[fn]['features']) for fn in pkgs)
-        key = fsd, len(pkgs)
-        #print key, pkgs
-        candidates[key].append(pkgs)
-
-    if not candidates:
-        print "Error: UNSAT"
-        return []
-
-    minkey = min(candidates)
-
-    mc = candidates[minkey]
-    if len(mc) != 1:
-        print 'WARNING:', len(mc), root_dists, features
-
-    return candidates[minkey][0]
-
-
-verscores = {}
-def select_dists_spec(spec):
-    mspec = spec.replace('=', ' ')
-    if spec.count('=') == 1:
-        mspec += '*'
-    ms = MatchSpec(mspec)
-
-    pkgs = sorted(get_pkgs(ms))
-    vs = 0
-    for p1, p2 in iter_pairs(pkgs):
-        verscores[p1.fn] = vs
-        if p2 and p2 > p1:
-            vs += 1
-    #pprint(verscores)
-    return [p.fn for p in pkgs]
-
-@memoized
-def sum_matches(fn1, fn2):
-    return sum(ms.match(fn2) for ms in index[fn1]['ms_depends'])
-
-def select_root_dists(specs, features, installed):
-    args = [select_dists_spec(spec) for spec in specs]
-
-    @memoized
-    def installed_matches(fn):
-        return sum(sum_matches(fn, fn2) for fn2 in installed)
-
-    candidates = defaultdict(list)
-    for dists in itertools.product(*args):
-        fsd = olx = svs = sim = 0
         for fn1 in dists:
-            fsd += len(features ^ index[fn1]['features'])
-            olx += sum(sum_matches(fn1, fn2) for fn2 in dists if fn1 != fn2)
-            svs += verscores[fn1]
-            sim += installed_matches(fn1)
+            for ms in self.ms_depends(fn1):
+                clause = [-v[fn1]]
+                for fn2 in self.find_matches(ms):
+                    if fn2 in dists:
+                        clause.append(v[fn2])
 
-        key = -fsd, olx, svs, sim
-        #print dists, key
-        candidates[key].append(dists)
+                assert len(clause) > 1, fn1
+                clauses.append(clause)
 
-    maxkey = max(candidates)
-    #print 'maxkey:', maxkey
+        for root_fn in root_dists:
+            clauses.append([v[root_fn]])
 
-    mc = candidates[maxkey]
-    if len(mc) != 1:
-        print 'WARNING:', len(mc)
-        for c in mc:
-            print '\t', c
+        candidates = defaultdict(list)
+        for sol in pycosat.itersolve(clauses):
+            pkgs = [w[lit] for lit in sol if lit > 0]
+            fsd = sum(len(features ^ self.features(fn)) for fn in pkgs)
+            key = fsd, len(pkgs)
+            #print key, pkgs
+            candidates[key].append(pkgs)
 
-    return set(candidates[maxkey][0])
+        if not candidates:
+            print "Error: UNSAT"
+            return []
 
-def update_with_features(fn, features):
-    info = index[fn]
-    for fs, depends_updates in info.get('with_features', {}).iteritems():
-        if not set(fs.split()).issubset(features):
-            continue
-        updates = {ms.name: ms for ms in [MatchSpec(mspec)
-                                          for mspec in depends_updates]}
-        for i, ms in enumerate(info['ms_depends']):
-            if ms.name in updates:
-                info['ms_depends'][i] = updates[ms.name]
+        minkey = min(candidates)
 
-def solve(specs, features, installed, verbose=False):
-    dists = select_root_dists(specs, features, installed)
+        mc = candidates[minkey]
+        if len(mc) != 1:
+            print 'WARNING:', len(mc), root_dists, features
+
+        return candidates[minkey][0]
+
+    verscores = {}
+    def select_dists_spec(self, spec):
+        mspec = spec.replace('=', ' ')
+        if spec.count('=') == 1:
+            mspec += '*'
+        ms = MatchSpec(mspec)
+
+        pkgs = sorted(self.get_pkgs(ms))
+        vs = 0
+        for p1, p2 in iter_pairs(pkgs):
+            self.verscores[p1.fn] = vs
+            if p2 and p2 > p1:
+                vs += 1
+        #pprint(self.verscores)
+        return [p.fn for p in pkgs]
+
+    @memoize
+    def sum_matches(self, fn1, fn2):
+        return sum(ms.match(fn2) for ms in self.ms_depends(fn1))
+
+    def select_root_dists(self, specs, features, installed):
+        args = [self.select_dists_spec(spec) for spec in specs]
+
+        @memoized
+        def installed_matches(fn):
+            return sum(self.sum_matches(fn, fn2) for fn2 in installed)
+
+        candidates = defaultdict(list)
+        for dists in itertools.product(*args):
+            fsd = olx = svs = sim = 0
+            for fn1 in dists:
+                fsd += len(features ^ self.features(fn1))
+                olx += sum(self.sum_matches(fn1, fn2)
+                           for fn2 in dists if fn1 != fn2)
+                svs += self.verscores[fn1]
+                sim += installed_matches(fn1)
+
+            key = -fsd, olx, svs, sim
+            #print dists, key
+            candidates[key].append(dists)
+
+        maxkey = max(candidates)
+        #print 'maxkey:', maxkey
+
+        mc = candidates[maxkey]
+        if len(mc) != 1:
+            print 'WARNING:', len(mc)
+            for c in mc:
+                print '\t', c
+
+        return set(candidates[maxkey][0])
+
+    def update_with_features(self, fn, features):
+        info = self.index[fn]
+        for fs, depends_updates in info.get('with_features', {}).iteritems():
+            if not set(fs.split()).issubset(features):
+                continue
+            updates = {ms.name: ms for ms in [MatchSpec(mspec)
+                                              for mspec in depends_updates]}
+            for i, ms in enumerate(self.msd[fn]):
+                if ms.name in updates:
+                    self.msd[fn][i] = updates[ms.name]
+
+def solve(index, specs, features, installed, verbose=False):
+    r = Resolve(index)
+    dists = r.select_root_dists(specs, features, installed)
     for fn in dists:
         track_features = set(index[fn].get('track_features', '').split())
         features.update(track_features)
     if verbose:
         print dists, features
     for fn in dists:
-        update_with_features(fn, features)
-    return solve2(dists, features, verbose)
+        r.update_with_features(fn, features)
+    return r.solve2(dists, features, verbose)
 
 
 def test_all():
@@ -259,7 +266,8 @@ if __name__ == '__main__':
     if len(args) == 0:
         test_all()
     else:
-        #index = get_index()
+        idx = get_index()
         features = set(['mkl']) if opts.mkl else set()
-        installed = solve2({'anaconda-1.5.0-np17py27_0.tar.bz2'}, set())
-        pprint(solve(args, features, installed, verbose=True))
+        installed = ['numpy-1.7.1-py27_0.tar.bz2',
+                     'python-2.7.5-0.tar.bz2']
+        pprint(solve(idx, args, features, installed, verbose=True))
