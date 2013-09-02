79f3c83f3af70a692ea64837b5baf361e89fee87
Ilan Schnell <ilanschnell@gmail.com>
2013-09-01 19:05:05 -0500

    use conda.connection

--- conda/s2.py
+++ conda/s2.py
@@ -1,11 +1,15 @@
 import urllib2
 
+from conda.connection import connectionhandled_urlopen
+
+
 url = 'http://repo.continuum.io/pkgs/gpl/osx-64/repodata.json'
+#url = 'https://conda.binstar.org/ilan/osx-64/repodata.json'
 
 request = urllib2.Request(url)
 request.add_header('If-None-Match', '2f1bf63044f924c048e0dce972929c4b')
 try:
-    u = urllib2.urlopen(request)
+    u = connectionhandled_urlopen(request)
 except urllib2.HTTPError as e:
     print e.code, e.msg
 except urllib2.URLError:
