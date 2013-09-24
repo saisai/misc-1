45b2b0e6607ea489a42b0b6eab7bf73ec3ae7179
Ilan Schnell <ilanschnell@gmail.com>
2013-09-23 22:14:04 -0500

    add files

---
+++ utf-8.py
@@ -0,0 +1,16 @@
+from bitarray import bitarray
+
+
+def f(s):
+    s = s.replace(' ', '')
+    a = bitarray(s)
+    print a
+    b = a.tobytes()
+    print repr(b)
+    c = b.decode('utf-8')
+    print c
+
+f('010 00001')
+f('110 01111  10 000001')
+f('1110 0111  10 100001  10 100111')
+
