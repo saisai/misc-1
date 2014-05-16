60a24f17b7be5443c1217de0a2dd6c8fa8dd9628
Ilan Schnell <ilanschnell@gmail.com>
2014-05-16 14:14:27 -0500

    formatting

--- conda/phone.py
+++ conda/phone.py
@@ -1,15 +1,23 @@
 import socket
 
-host, port = 'binstar.org', 80
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-req = '''\
+HOST, PORT = 'binstar.org', 80
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    req = '''\
 POST /phone-home HTTP/1.1
 Host: %s
 
-installer_name=Anaconda&installer_version=2.0.0&platform=linux-64
-''' % host
-s.send(req.replace('\n', '\r\n'))
+name=__NAME__&version=__VERSION__&platform=__PLATFORM__
+''' % HOST
+    print req
+    s.send(req.replace('\n', '\r\n'))
 
-print s.recv(1024).replace('\r', r'\r')
+    print s.recv(1024).replace('\r', r'\r')
+
+
+if __name__ == '__main__':
+    main()
