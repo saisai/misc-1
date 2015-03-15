11ff6183bf7a64d51e6ec192d0f52d77a7bd3f16
Ilan Schnell <ilanschnell@gmail.com>
2015-03-15 11:05:06 -0500

    add enw file

---
+++ conda/sign-old.py
@@ -0,0 +1,66 @@
+import json
+import shutil
+import hashlib
+import tarfile
+import tempfile
+from os.path import join
+
+from Crypto.PublicKey import RSA
+
+from ll.utils import tar_update
+
+
+def info_m(t, m):
+    if m.isfile():
+        h = hashlib.new('sha256')
+        f = t.extractfile(m.path)
+        while True:
+            chunk = f.read(262144)
+            if not chunk:
+                break
+            h.update(chunk)
+        return h.hexdigest()
+    if m.isdir():
+        return 'DIR'
+    if m.issym():
+        return '-> %s' % m.linkname
+    return '<UNKNOWN>'
+
+def list_t(t):
+    for m in sorted(t.getmembers(), key=lambda m: m.path):
+        if m.path == 'info/signatures.json':
+            continue
+        yield '%s %s\n' % (m.path, info_m(t, m))
+
+def sha256_t(t):
+    h = hashlib.new('sha256')
+    for line in list_t(t):
+        h.update(line)
+    return h
+
+def read_signatures_t(t):
+    try:
+        return json.load(t.extractfile('info/signatures.json'))
+    except (KeyError, AttributeError):
+        return {}
+
+def sign(path, keyname):
+    key = RSA.importKey(open('%s.priv' % keyname).read())
+    t = tarfile.open(path)
+    h = sha256_t(t)
+    signatures = read_signatures_t(t)
+    t.close()
+    sig = key.sign(h.digest(), '')[0]
+    signatures[keyname] = str(sig)
+
+    tmp_dir = tempfile.mkdtemp()
+    sig_path = join(tmp_dir, 'signatures.json')
+    with open(sig_path, 'w') as fo:
+        json.dump(signatures, fo, indent=2, sort_keys=True)
+    tar_update(path, {'info/signatures.json': sig_path})
+    shutil.rmtree(tmp_dir)
+
+#sign("python-2.7.4-1.tar.bz2", 'cio')
+t = tarfile.open("python-2.7.4-1.tar.bz2")
+print read_signatures_t(t)
+t.close()
