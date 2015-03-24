4cc8fa9fd1f1f446b544ec34abf34755fc124a51
Ilan Schnell <ilanschnell@gmail.com>
2015-03-24 01:30:27 -0500

    add output

--- c/strncpy.c
+++ c/strncpy.c
@@ -5,10 +5,11 @@
 int main()
 {
     int i;
-    char f[] = "aaaaaaaaaaaaaaaaaa";
+    char f[] = "aaaaaaaaaaaa";
 
     strncpy(f + 2, "1234", 4);
 
+    printf("\"%s\"\n", f);
     for (i = 0; i < 10; i++)
         printf("%d '%c' %d\n", i, *(f + i), *(f + i));
 
