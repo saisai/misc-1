fa90da56cc0f7fd2dc62a643ec71a99978ef257d
Ilan Schnell <ilanschnell@gmail.com>
2015-03-24 02:20:03 -0500

    minor improvements

--- c/strncpy.c
+++ c/strncpy.c
@@ -7,11 +7,11 @@ int main()
     int i;
     char f[] = "aaaaaaaaaaaa";
 
-    strncpy(f + 2, "1234", 4);
+    strncpy(f + 2, "123456", 4);
 
     printf("\"%s\"\n", f);
     for (i = 0; i < 10; i++)
-        printf("%d '%c' %d\n", i, *(f + i), *(f + i));
+        printf("%d '%c' %d\n", i, f[i], f[i]);
 
     return 0;
 }
