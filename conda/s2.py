189509fbb918bacfbc60e84b5fbd59e28627ac90
Ilan Schnell <ilanschnell@gmail.com>
2015-04-04 10:46:36 -0500

    update example

--- conda/s2.py
+++ conda/s2.py
@@ -1,23 +1,13 @@
-import urllib2
-
-from conda.connection import connectionhandled_urlopen
+from pprint import pprint
+from conda.connection import CondaSession
 
 
 url = 'http://repo.continuum.io/pkgs/gpl/osx-64/repodata.json'
 #url = 'https://conda.binstar.org/ilan/osx-64/repodata.json'
 
-request = urllib2.Request(url)
-request.add_header('If-None-Match', '2f1bf63044f924c048e0dce972929c4b')
-try:
-    u = connectionhandled_urlopen(request)
-except urllib2.HTTPError as e:
-    print e.code, e.msg
-except urllib2.URLError:
-    print "host unknown"
 
-try:
-    content = u.read()
-    u.close()
-    print content
-except NameError:
-    pass
+session = CondaSession()
+resp = session.get(url,
+                   headers={'If-None-Match': '"551f32ee-9b"'})
+print resp.status_code
+pprint(dict(resp.headers))
