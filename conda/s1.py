9b97aff48e9bb29bf1cfa1946c26b57a2f11957f
Ilan Schnell <ilanschnell@gmail.com>
2015-04-05 18:42:16 -0500

    change lines

--- conda/s1.py
+++ conda/s1.py
@@ -2,9 +2,6 @@
 import socket
 
 host, port = 'repo.continuum.io', 80
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
 req = '''\
 GET /pkgs/gpl/osx-64/repodata.json HTTP/1.1
 Host: %s
@@ -13,6 +10,8 @@ If-Modified-Since: Sat, 04 Apr 2015 00:40:14 GMT
 If-None-Match: "551f32ee-9b"
 
 ''' % host
-s.send(req.replace('\n', '\r\n'))
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.send(req.replace('\n', '\r\n'))
 print s.recv(1024).replace('\r', r'\r')
