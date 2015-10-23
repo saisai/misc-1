a05e92b510c394e671eaa15a3807000edbb43fec
Ilan Schnell <ilanschnell@gmail.com>
2015-10-22 21:23:20 -0500

    update test program

--- conda/t.py
+++ conda/t.py
@@ -1,43 +1,32 @@
-import base64
+from conda.api import get_index
+from conda.plan import add_defaults_to_specs
+from conda.resolve import Resolve
+from conda.cli.common import specs_from_args
 
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-from Crypto import Random
 
 
-def sig2ascii(i):
-    ret = []
-    while i:
-        i, r = divmod(i, 256)
-        ret.append(r)
-    return base64.b64encode(''.join(chr(n) for n in ret[::-1]))
+def main():
+    from optparse import OptionParser
 
-def ascii2sig(s):
-    res = 0
-    for c in base64.b64decode(s):
-        res *= 256
-        res += ord(c)
-    return res
+    p = OptionParser(usage="usage: %prog SPEC [SPEC ...]",
+                     description="""\
+creates explicit file from SPECs.""")
 
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)
-private_key_text = key.exportKey()
-public_key_text = key.publickey().exportKey()
-print private_key_text
-print public_key_text
+    opts, args = p.parse_args()
 
-k2 = RSA.importKey(private_key_text)
-assert k2.publickey().exportKey() == public_key_text
+    specs = specs_from_args(args)
+    index = get_index(())
+    r = Resolve(index)
+    add_defaults_to_specs(r, [], specs)
+    dists = list(r.solve(specs))
 
-cnt = 0
-while 1:
-    cnt += 1
-    data = Random.new().read(100)
-    hash = SHA256.new(data).digest()
-    sig = key.sign(hash, '')[0]
-    #print sig
-    s = sig2ascii(sig)
-    #print s
-    assert ascii2sig(s) == sig, sig
-    assert key.verify(hash, (ascii2sig(s),))
-    print cnt
+    print
+    print
+    print '@EXPLICIT'
+    for dist in dists:
+        url = index[dist]['channel'] + dist
+        print url
+
+
+if __name__ == '__main__':
+    main()
