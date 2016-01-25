f553043acfb0fe3c60ddcfe0e32c66498a7ec96f
Ilan Schnell <ilanschnell@gmail.com>
2016-01-24 23:29:18 -0600

    update code

--- pil/video.py
+++ pil/video.py
@@ -1,35 +1,32 @@
-from string import uppercase
-from random import randint, choice
+from random import randint
 
-from PIL import ImageDraw, ImageFont, Image
+from PIL import ImageDraw, Image
 import cv2
 import numpy
 
 
-path = '/Users/ilan/python/pkgs/matplotlib-1.5.1-np110py27_0/lib/python2.7/site-packages/matplotlib/mpl-data/fonts/ttf/Vera.ttf'
-font = ImageFont.truetype(path, 25)
-
-def letterize(im, n=10000):
-    d = ImageDraw.Draw(im)
-    w, h = im.size
-    for unused in xrange(n):
-        x = randint(0, w-1)
-        y = randint(0, h-1)
-        c = a.getpixel((x, y))
-        d.text((x-10, y-10), choice(uppercase), fill=c, font=font)
-
-
-width, height = 800, 600
+width, height = 600, 800
+frames = 600
 
 video = cv2.VideoWriter('out.avi',
                         cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
                         20, (width, height), True)
 
-a = Image.open('/Users/ilan/Downloads/heather-hope.jpg')
-for t in xrange(200):
+a = Image.open('/Users/ilan/Desktop/moit.png')
+x0, y0 = 100, 300
+r = 10
+
+a = a.crop(box=(x0, y0, x0 + width, y0 + height))
+for t in xrange(frames):
     print t
-    b = a.resize((width, height))
-    letterize(b, 10 * t)
+    b = a.copy()
+    d = ImageDraw.Draw(b)
+    for unused in xrange((frames-1-t) * 10):
+        x = randint(0, width - 1)
+        y = randint(0, height - 1)
+        c = b.getpixel((x, y))
+        d.ellipse((x-r, y-r, x+r, y+r), fill=c)
+
     c = numpy.array(b.convert('RGB'))
     c = c[:, :, ::-1].copy()
     video.write(c)
