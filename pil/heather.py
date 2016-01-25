ec3ac338c53ce49a491b836591a748cc4325b238
Ilan Schnell <ilanschnell@gmail.com>
2016-01-24 20:39:06 -0600

    add teo functions

--- pil/heather.py
+++ pil/heather.py
@@ -1,32 +1,28 @@
 from PIL import Image
 
 
-X, Y = 3, 2
+def four_flip(im):
+    a = im.copy()
+    b = im.transpose(Image.FLIP_LEFT_RIGHT)
+    c = im.transpose(Image.FLIP_TOP_BOTTOM)
+    d = im.rotate(180)
+    w, h = a.size
+    res = im.resize((2 * w, 2 * h))
+    res.paste(a, box=(0, 0))
+    res.paste(b, box=(w, 0))
+    res.paste(c, box=(0, h))
+    res.paste(d, box=(w, h))
+    return res
 
-#im = open('/Users/ilan/cas-installer/cas_installer/nsis/continuum_nsis_header.bmp')
-im = Image.open('/Users/ilan/Downloads/heather-hope.jpg')
-im = im.resize((200, 300))
+def repeat(im, X=1, Y=1):
+    w, h = im.size
+    res = im.resize((X * w, Y * h))
+    for x in range(X):
+        for y in range(Y):
+            res.paste(im, box=(x * w, y * h))
+    return res
 
-a = im.copy()
-b = im.transpose(Image.FLIP_LEFT_RIGHT)
-c = im.transpose(Image.FLIP_TOP_BOTTOM)
-d = im.rotate(180)
 
-w, h = a.size
-im2 = im.resize((2 * w, 2 * h))
-print a.size
-print im2.size
-
-im2.paste(a, box=(0, 0))
-im2.paste(b, box=(w, 0))
-im2.paste(c, box=(0, h))
-im2.paste(d, box=(w, h))
-
-w, h = im2.size
-im3 = im2.resize((X * w, Y * h))
-for x in range(X):
-    for y in range(Y):
-        im3.paste(im2, box=(x * w, y * h))
-
-im3.show()
-#im3.save('hope.png')
+im = Image.open('/Users/ilan/Downloads/heather-hope.jpg').resize((200, 300))
+im2 = repeat(four_flip(im), 3, 2)
+im2.show()
