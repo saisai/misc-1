e314a6897dfa0b2a6a345185dad8bf9e6867872e
Ilan Schnell <ilanschnell@gmail.com>
2016-02-20 21:19:03 -0600

    update work

--- conda/c2r.py
+++ conda/c2r.py
@@ -5,8 +5,10 @@ import json
 import shutil
 from collections import defaultdict
 from os.path import isdir, isfile, join, getsize
+from pprint import pprint
 
 from conda import config
+from conda.utils import md5_file
 
 
 pkgs_dir = config.pkgs_dirs[0]
@@ -33,40 +35,56 @@ def read_cached_repodata(): # populates 'crd'
 def find_info(fn):
     if fn not in crd:
         return None
-    size = getsize(join(pkgs_dir, fn))
+    path = join(pkgs_dir, fn)
+    md5 = md5_file(path)
     for info in crd[fn]:
-        if info.get('size') == size:
-            assert '%(name)s-%(version)s-%(build)s.tar.bz2' % info == fn, fn
+        if md5 == info.get('md5'):
             return info
     return None
-
-def create_new_index():
-    read_cached_repodata()
-    index = {}
-    for fn in os.listdir(pkgs_dir):
-        if not fn.endswith('.tar.bz2'):
-            continue
-        info = find_info(fn)
-        if info:
-            index[fn] = info
-    return index
 
 def create_repo():
     if not isdir(repo_path):
         os.makedirs(repo_path)
-    index = create_new_index()
-    for fn, info in index.iteritems():
-        dst = join(repo_path, fn)
-        if isfile(dst) and getsize(dst) == info.get('size'):
+    try:
+        d = json.load(open(repodata_path))
+        index = d['packages']
+    except IOError:
+        index = {}
+    read_cached_repodata()
+    for fn in os.listdir(pkgs_dir):
+        if not fn.endswith('.tar.bz2') or fn in index:
+            continue
+        info = find_info(fn)
+        if not info:
             continue
         sys.stdout.write('.')
         sys.stdout.flush()
-        src = join(pkgs_dir, fn)
-        shutil.copyfile(src, dst)
+        shutil.copyfile(join(pkgs_dir, fn),
+                        join(repo_path, fn))
+        index[fn] = info
+
     repodata = {'packages': index}
     with open(repodata_path, 'w') as fo:
         json.dump(repodata, fo, indent=2, sort_keys=True)
     print
 
+def test_repo():
+    d = json.load(open(repodata_path))
+    index = d['packages']
+    files = set()
+    for fn in os.listdir(repo_path):
+        if not fn.endswith('.tar.bz2'):
+            continue
+        files.add(fn)
+        info = index[fn]
+        path = join(repo_path, fn)
+        assert '%(name)s-%(version)s-%(build)s.tar.bz2' % info == fn
+        assert getsize(path) == info['size']
+        assert md5_file(path) == info['md5']
+
+    assert files == set(index)
+
+
 if __name__ == '__main__':
     create_repo()
+    test_repo()
