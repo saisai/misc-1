a5ef23d7c3a50c766ee98936960fbfe69b0e8164
Ilan Schnell <ilanschnell@gmail.com>
2016-02-24 23:47:58 -0600

    update example

--- conda/s1.py
+++ conda/s1.py
@@ -3,14 +3,14 @@ import socket
 
 host, port = 'repo.continuum.io', 80
 req = '''\
-GET /pkgs/gpl/osx-64/repodata.json HTTP/1.1
+GET /pkgs/rss.xml HTTP/1.1
 Host: %s
 Connection: keep-alive
-If-Modified-Since: Mon, 06 Apr 2015 18:33:45 GMT\r
-If-None-Match: "5522d189-9b"
 User-Agent: Test-User-Agent/1.0.0
 
 ''' % host
+#If-Modified-Since: Mon, 06 Apr 2015 18:33:45 GMT\r
+#If-None-Match: "5522d189-9b"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
