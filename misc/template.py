369d07a6a151db83258746acb28cbed1f6d378f0
Ilan Schnell <ilanschnell@gmail.com>
2016-02-28 03:01:11 -0600

    ignore non-existing keys

--- misc/template.py
+++ misc/template.py
@@ -2,15 +2,12 @@ import re
 import sys
 
 
-def simple_template(data, d):
+def fill_template(data, d):
     pat = re.compile(r'__(\w+)__')
 
     def replace(match):
         key = match.group(1)
-        try:
-            return d[key]
-        except KeyError:
-            sys.exit("Error: did not find key '%s' in dict" % key)
+        return d.get(key, match.group(0))
 
     return pat.sub(replace, data)
 
@@ -20,7 +17,9 @@ if __name__ == '__main__':
 My name is __NAME__!
 I am __AGE__ years old.
 
+__FOO__
+
 Sincerely __NAME__
 """
-    print simple_template(DATA,
-                          {'NAME': 'Ilan', 'AGE': '44', 'SEX': 'male'})
+    print fill_template(DATA,
+                        {'NAME': 'Ilan', 'AGE': '44', 'SEX': 'male'})
