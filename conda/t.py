d703626b4876295b3f72e07b62c9bd4d24890b90
Ilan Schnell <ilanschnell@gmail.com>
2016-05-31 17:00:20 -0500

    update test script

--- conda/t.py
+++ conda/t.py
@@ -6,4 +6,6 @@ from os.path import join
 prefix = join(os.getcwd(), 500 * 'a')[:260]
 print len(prefix)
 
-call(['conda', 'create', '-p', prefix, 'openssl'])
+env = dict(os.environ)
+env['CIO_TEST'] = '3'
+call(['conda', 'create', '-p', prefix, 'openssl'], env=env)
