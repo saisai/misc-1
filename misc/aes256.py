c68deaf3f05d08d132a6bf4140e0ddf8ffc01ecb
Ilan Schnell <ilanschnell@gmail.com>
2016-06-12 15:34:49 -0500

    update work

--- misc/aes256.py
+++ misc/aes256.py
@@ -1,11 +1,10 @@
-import base64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 
 
-assert AES.block_size == 16
 BS = AES.block_size
+assert BS == 16
 KEY = hashlib.sha256('PassW0rd').digest()
 
 
@@ -19,21 +18,19 @@ def unpad(s):
 
 def encrypt(raw):
     raw = pad(raw)
-    iv = Random.new().read(AES.block_size)
+    iv = Random.new().read(BS)
     cipher = AES.new(KEY, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
+    return iv + cipher.encrypt(raw)
 
 def decrypt(enc):
-    enc = base64.b64decode(enc)
     iv = enc[:BS]
     cipher = AES.new(KEY, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(enc[BS:])).decode('utf-8')
 
 
-msg = "Hello World!1234"
-print len(msg)
-print repr(pad(msg))
-print unpad(pad(msg))
-enc = repr(encrypt(msg))
-print enc
-print decrypt(enc)
+for n in range(20):
+    msg = ''.join(chr(i) for i in range(n))
+    print len(msg),
+    enc = encrypt(msg)
+    print len(enc)
+    assert decrypt(enc) == msg
