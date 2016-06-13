cfcd59d5acef5dcb4f1bcea59870738113f1bf93
Ilan Schnell <ilanschnell@gmail.com>
2016-06-13 01:35:16 -0500

    working AES example

--- embed/main.c
+++ embed/main.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    PyObject *d, *m, *v, *bar;
+    PyObject *d, *m, *v, *res;
+    FILE *fp;
+    char nm[512];
 
     Py_SetProgramName(argv[1]);  /* optional but recommended */
     Py_Initialize();
@@ -15,18 +17,25 @@ int main(int argc, char *argv[])
                        PyImport_ImportModule("__builtin__"));
 
     d = PyModule_GetDict(m);
-    PyDict_SetItemString(d, "foo", PyInt_FromLong(42));
-    v = PyRun_String("import os\n"
-                     "from Crypto import Random\n"
-                     "print os, foo\n"
-                     "bar = foo ** 3\n",
-                     Py_file_input, d, d);
+
+    fp = fopen("enc.x", "rb");
+    fread(nm, sizeof(char), 304, fp);
+    PyDict_SetItemString(d, "enc", PyString_FromStringAndSize(nm, 304));
+    v = PyRun_String(
+            "import hashlib\n"
+            "from Crypto.Cipher import AES\n"
+            "KEY = hashlib.sha256(b'PassW0rd').digest()\n"
+            "cipher = AES.new(KEY, AES.MODE_CBC, enc[:16])\n"
+            "res = cipher.decrypt(enc[16:])\n"
+            "pad_len = ord(res[len(res) - 1:])\n"
+            "res = res[:-pad_len]\n",
+            Py_file_input, d, d);
     if (v == NULL) {
         PyErr_Print();
         return 1;
     }
-    bar = PyDict_GetItemString(d, "bar");
-    printf("RESULT: %ld\n", PyLong_AsLong(bar));
+    printf("RESULT: %s\n",
+           PyString_AsString(PyDict_GetItemString(d, "res")));
     Py_DECREF(v);
 
     Py_Finalize();
