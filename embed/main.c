e4d0d3bbaa32d21552f639edd778e9dca60ad63c
Ilan Schnell <ilanschnell@gmail.com>
2016-06-13 01:05:56 -0500

    extend example

--- embed/main.c
+++ embed/main.c
@@ -1,29 +1,33 @@
 #include <Python.h>
 
+
 int main(int argc, char *argv[])
 {
-    PyObject *d, *m, *v;
+    PyObject *d, *m, *v, *bar;
 
-    Py_SetProgramName(argv[0]);  /* optional but recommended */
+    Py_SetProgramName(argv[1]);  /* optional but recommended */
     Py_Initialize();
 
-    m = PyImport_AddModule("cio_128275909");
+    m = PyImport_AddModule("wnci128275909");
     if (m == NULL)
         return 1;
     PyModule_AddObject(m, "__builtins__",
                        PyImport_ImportModule("__builtin__"));
 
     d = PyModule_GetDict(m);
+    PyDict_SetItemString(d, "foo", PyInt_FromLong(42));
     v = PyRun_String("import os\n"
-                     "print os\n"
-                     "from Crypto import Random\n",
+                     "from Crypto import Random\n"
+                     "print os, foo\n"
+                     "bar = foo ** 3\n",
                      Py_file_input, d, d);
     if (v == NULL) {
         PyErr_Print();
         return 1;
     }
+    bar = PyDict_GetItemString(d, "bar");
+    printf("RESULT: %ld\n", PyLong_AsLong(bar));
     Py_DECREF(v);
-
 
     Py_Finalize();
     return 0;
--- embed/make.sh
+++ embed/make.sh
@@ -5,4 +5,4 @@ PP=$HOME/python
 gcc -I $PP/include/python2.7 -L $PP/lib -l python2.7 main.c
 
 export DYLD_LIBRARY_PATH=$PP/lib
-./a.out
+./a.out $PP/bin/python
