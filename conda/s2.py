304051a248bb149e4bef1296aa23769d52ebe4d0
Ilan Schnell <ilanschnell@gmail.com>
2016-06-25 14:53:10 -0500

    update test

--- conda/s2.py
+++ conda/s2.py
@@ -2,12 +2,11 @@ from pprint import pprint
 from conda.connection import CondaSession
 
 
-url = 'http://repo.continuum.io/pkgs/free/osx-64/repodata.json'
-url = 'https://conda.anaconda.org/ilan/osx-64/repodata.json'
-
+url = 'http://repo.continuum.io/pkgs/free/osx-64/repodata.json.bz2'
+url = 'https://conda.anaconda.org/conda-forge/osx-64/repodata.json.bz2'
 
 session = CondaSession()
 resp = session.get(url,
-                   headers={'If-None-Match': '"551f32ee-9b"'})
+                   headers={'If-None-Match': '"576d9600-461a7"'})
 print resp.status_code
 pprint(dict(resp.headers))
