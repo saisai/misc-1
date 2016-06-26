7e7d459618978a58f10d674c793717811050db98
Ilan Schnell <ilanschnell@gmail.com>
2016-06-26 11:16:21 -0500

    update test script

--- conda/s2.py
+++ conda/s2.py
@@ -1,12 +1,18 @@
+import time
+import random
 from pprint import pprint
+
 from conda.connection import CondaSession
 
 
 url = 'http://repo.continuum.io/pkgs/free/osx-64/repodata.json.bz2'
 url = 'https://conda.anaconda.org/conda-forge/osx-64/repodata.json.bz2'
 
-session = CondaSession()
-resp = session.get(url,
-                   headers={'If-None-Match': '"576d9600-461a7"'})
-print resp.status_code
-pprint(dict(resp.headers))
+while 1:
+    startTime = time.time()
+    session = CondaSession()
+    resp = session.get(url)
+    timeTaken = time.time() - startTime
+    print resp.status_code, timeTaken
+    #pprint(dict(resp.headers))
+    time.sleep(random.random() * 10.0)
