19036e9bee9f5417d4e7913cc9301a82e3eccc2c
Ilan Schnell <ilan@continuum.io>
2016-10-28 14:54:21 -0500

    update test script

--- conda/s1.py
+++ conda/s1.py
@@ -7,9 +7,10 @@ GET /pkgs/rss.xml HTTP/1.1
 Host: %s
 Connection: keep-alive
 User-Agent: Test-User-Agent/1.0.0
-If-Modified-Since: Thu, 20 Oct 2016 15:52:04 GMT
+If-None-Match: "5813a983-2e3a"
 
 ''' % host
+#If-Modified-Since: Thu, 20 Oct 2016 15:52:04 GMT
 #If-None-Match: "5522d189-9b"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
