b6fec62bfff652367ee3dadcaa996da323746e0e
Ilan Schnell <ilanschnell@gmail.com>
2017-01-22 19:04:00 -0600

    update work

--- repo/repack.py
+++ repo/repack.py
@@ -1,8 +1,6 @@
-import sys
-import json
 import shutil
 import tempfile
-from os.path import basename, isdir, join
+from os.path import isdir, join
 
 from ll.utils import tar_cf, tar_xf, rm_rf
 from bt.build import tar_recipe
@@ -16,16 +14,29 @@ def repack(tar_path):
     tar_xf(tar_path, tmp_dir)
     assert isdir(info_dir)
 
-    for fn in 'git', 'files.json', 'recipe.json', 'requires':
+    for fn in 'git', 'files.json', 'recipe.json':
         rm_rf(join(info_dir, fn))
     tar_recipe(info_dir)
 
-    tar_cf(basename(tar_path), tmp_dir)
+    tar_cf(tar_path, tmp_dir, mode='w:bz2')
     shutil.rmtree(tmp_dir)
 
 
 def main():
-    repack(sys.argv[1])
+    from optparse import OptionParser
+
+    p = OptionParser(
+        usage="usage: %prog [options] TAR [TAR ...]",
+        description="repack conda packages")
+
+    opts, args = p.parse_args()
+
+    for path in args:
+        if not path.endswith('.tar.bz2'):
+            print 'ignoring:', path
+            continue
+        repack(path)
+        validate_package(path)
 
 
 if __name__ == '__main__':
