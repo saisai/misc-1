ab8af7ba6ce8e9503170d5d59eb95396b55f1c2f
Ilan Schnell <ilanschnell@gmail.com>
2017-01-23 19:02:39 -0600

    update work

--- repo/repack.py
+++ repo/repack.py
@@ -4,13 +4,14 @@ import tempfile
 from pprint import pprint
 from os.path import basename, dirname, join
 
-from ll.utils import tar_cf, tar_xf, rm_rf
+from ll.utils import tar_cf, tar_xf, rm_rf, memoized
 from ll.diffutils import show_dict_diff
 from bt.build import tar_recipe
 
 from anaconda_verify.package import validate_package
 
 
+@memoized
 def meta_from_repodata(tar_path):
     repodata = json.load(open(join(dirname(tar_path), 'repodata.json')))
     index = repodata['packages']
@@ -18,6 +19,20 @@ def meta_from_repodata(tar_path):
     for key in 'date', 'md5', 'size':
         del meta[key]
     return meta
+
+
+def cleanup_info_dir(info_dir):
+    for fn in 'git', 'files.json', 'recipe.json':
+        rm_rf(join(info_dir, fn))
+    tar_recipe(info_dir)
+
+
+def update_meta(meta1, meta2):
+    for key in 'name', 'version', 'build', 'build_number':
+        assert meta1[key] == meta2[key], key
+
+    if set(meta2.get('depends')) != set(meta1['depends']):
+        meta2['depends'] = meta1['depends']
 
 
 def repack(tar_path):
@@ -32,15 +47,13 @@ def repack(tar_path):
     pprint(meta2)
 
     show_dict_diff(meta1, meta2)
+    update_meta(meta1, meta2)
+    pprint(meta2)
 
-    for key in 'name', 'version', 'build', 'build_number':
-        assert meta1[key] == meta2[key], key
+    with open(join(info_dir, 'index.json'), 'w') as fo:
+        json.dump(meta2, fo, indent=2, sort_keys=True)
 
-    # cleanup files in info/
-    for fn in 'git', 'files.json', 'recipe.json':
-        rm_rf(join(info_dir, fn))
-    tar_recipe(info_dir)
-
+    cleanup_info_dir(info_dir)
     tar_cf(basename(tar_path), tmp_dir, mode='w:bz2')
     shutil.rmtree(tmp_dir)
 
