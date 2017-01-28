c90a584d93089008c807d60d752e21987d0fef55
Ilan Schnell <ilanschnell@gmail.com>
2017-01-28 17:36:58 -0600

    rename variables

--- repo/main.py
+++ repo/main.py
@@ -4,9 +4,9 @@ from repo.utils import normalize_depends, meta_from_index
 
 
 cnt_tot = 0
-cnt_nodep = set()
-cnt_depend = set()
-cnt_lic = set()
+set_nodep = set()
+set_depend = set()
+set_lic = set()
 
 for path, unused_md5 in read_repodatas():
     meta2 = meta_from_index(path)
@@ -25,21 +25,21 @@ for path, unused_md5 in read_repodatas():
     cnt_tot += 1
 
     if 'depends' not in meta2:
-        cnt_nodep.add(path)
+        set_nodep.add(path)
 
     if depends1 != normalize_depends(meta2.get('depends', ['xyz'])):
-        cnt_depend.add(path)
+        set_depend.add(path)
 
     for key in 'license', 'license_family':
         if meta1.get(key) != meta2.get(key):
-            cnt_lic.add(path)
+            set_lic.add(path)
 
 print cnt_tot
-print 'no depends', len(cnt_nodep)
-print 'depends', len(cnt_depend)
-assert cnt_nodep <= cnt_depend
-print 'license', len(cnt_lic)
-print 'intersection', len(cnt_depend & cnt_lic)
-print 'union', len(cnt_depend | cnt_lic)
-print 'd - l', len(cnt_depend - cnt_lic)
-print 'l - d', len(cnt_lic - cnt_depend)
+print 'no depends', len(set_nodep)
+print 'depends', len(set_depend)
+assert set_nodep <= set_depend
+print 'license', len(set_lic)
+print 'intersection', len(set_depend & set_lic)
+print 'union', len(set_depend | set_lic)
+print 'd - l', len(set_depend - set_lic)
+print 'l - d', len(set_lic - set_depend)
