5481148ed689fb11ba49f1ec6aa7f464c94f1c3b
Ilan Schnell <ilanschnell@gmail.com>
2017-01-28 16:13:46 -0600

    generalize code

--- repo/main.py
+++ repo/main.py
@@ -3,7 +3,8 @@ from repo.repack import meta_from_repodata
 from repo.utils import normalize_depends, meta_from_index
 
 
-cnt = 0
+cnt_tot = cnt_depend = cnt_lic = cnt_dl = 0
+
 for path, unused_md5 in read_repodatas():
     meta2 = meta_from_index(path)
     if meta2 is None:
@@ -15,13 +16,23 @@ for path, unused_md5 in read_repodatas():
         print path, meta2['ctime']
 
     meta1 = meta_from_repodata(path)
-
     depends1 = meta1['depends']
     assert normalize_depends(depends1) == depends1
-    changes = bool(depends1 != normalize_depends(meta2.get('depends', [])))
-    for key in 'license', 'license_family':
-        changes = changes or (meta1.get(key) != meta2.get(key))
-    #print '%-60s %s' % (path[17:], changes)
-    cnt += int(bool(changes))
 
-print cnt
+    cnt_tot += 1
+
+    if depends1 != normalize_depends(meta2.get('depends', [])):
+        cnt_depend += 1
+        cnt_dl += 1
+
+    lc = False
+    for key in 'license', 'license_family':
+        if meta1.get(key) != meta2.get(key):
+            lc = True
+
+    if lc:
+        cnt_lic += 1
+        cnt_dl += 1
+
+for varname in 'cnt_tot', 'cnt_depend', 'cnt_lic', 'cnt_dl':
+    print '%s: %d' % (varname, eval(varname))
