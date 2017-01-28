12c5bd390b089624d3657e7e4d752f9669719533
Ilan Schnell <ilanschnell@gmail.com>
2017-01-28 00:27:14 -0600

    count changes

--- repo/main.py
+++ repo/main.py
@@ -3,6 +3,7 @@ from repo.repack import meta_from_repodata
 from repo.utils import normalize_depends, meta_from_index
 
 
+cnt = 0
 for path, unused_md5 in read_repodatas():
     meta2 = meta_from_index(path)
     if meta2 is None:
@@ -11,7 +12,10 @@ for path, unused_md5 in read_repodatas():
 
     depends1 = meta1['depends']
     assert normalize_depends(depends1) == depends1
-    changes = (depends1 != normalize_depends(meta2.get('depends', [])))
+    changes = bool(depends1 != normalize_depends(meta2.get('depends', [])))
     for key in 'license', 'license_family':
         changes = changes or (meta1.get(key) != meta2.get(key))
-    print '%-60s %s' % (path[17:], changes)
+    #print '%-60s %s' % (path[17:], changes)
+    cnt += int(bool(changes))
+
+print cnt
