3ed5b732fd412f0390661eeb8514908a9dad2c57
Ilan Schnell <ilanschnell@gmail.com>
2017-01-27 20:33:00 -0600

    update test script

--- repo/main.py
+++ repo/main.py
@@ -1,32 +1,32 @@
-from pprint import pprint
 import json
+from os.path import basename, dirname, join
 
-from ll.diffutils import show_set_diff, show_dict_diff
+from ll.utils import memoized
+from repo.filedb import read_repodatas
+from repo.repack import meta_from_repodata
+from repo.utils import normalize_depends
 
 
-index = json.load(open('index.json'))
-repodata = json.load(open('repodata.json'))['packages']
+@memoized
+def read_index(dir_path):
+    try:
+        return json.load(open(join(dir_path, 'index.json')))
+    except IOError:
+        return None
 
-print len(index), len(repodata)
+def meta_from_index(tar_path):
+    index = read_index(dirname(tar_path))
+    if index is None:
+        return None
+    return index[basename(tar_path)]
 
-for fn in sorted(index):
-    info1 = index[fn]
-    info2 = repodata[fn]
 
-    for key in 'depends', 'requires', 'license', 'license_family':
-        info1[key] = info2[key] = None
-    for key in 'arch', 'ucs', 'ctime', 'mtime', 'platform', 'subdir':
-        info1.pop(key, None)
-    for key in 'date',:
-        info2.pop(key, None)
+for path, unused_md5 in read_repodatas():
+    meta2 = meta_from_index(path)
+    if meta2 is None:
+        continue
+    meta1 = meta_from_repodata(path)
 
-    if info1 != info2:
-        print fn
-        show_dict_diff(info1, info2)
-
-    continue
-    dep1 = info1.get('depends', [])
-    dep2 = info2['depends']
-    if dep1 and dep1 != dep2:
-        print fn
-        show_set_diff(set(dep1), set(dep2))
+    changes = (meta1['depends'] !=
+               normalize_depends(meta2.get('depends', [])))
+    print '%-60s %s' % (path[17:], changes)
