eae22f73a207f005a41efff07f84638b61f36745
Ilan Schnell <ilanschnell@gmail.com>
2017-01-30 18:48:48 -0600

    improve output

--- repo/if.py
+++ repo/if.py
@@ -5,12 +5,13 @@ from collections import defaultdict
 with open('/home/ilan/anaconda/repo/filedb.json') as fi:
     d = json.load(fi)
 
-names = defaultdict(int)
+NAMES = defaultdict(int)
 
 for files in d.itervalues():
     for f in files:
         if f.startswith('info/'):
-            names[f] += 1
+            name = f.split('/')[1]
+            NAMES[name] += 1
 
-for f in sorted(names):
-    print f, names[f]
+for name in sorted(NAMES):
+    print name, NAMES[name]
