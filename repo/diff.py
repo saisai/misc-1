dd54c0159a98beb5dc0289739f1a629b93cec6ee
Ilan Schnell <ilanschnell@gmail.com>
2017-02-21 18:35:26 -0600

    remove debug output

--- repo/diff.py
+++ repo/diff.py
@@ -7,10 +7,8 @@ from repo.config import SUBDIRS
 for channel in 'free', 'pro':
     for subdir in list(SUBDIRS) + ['noarch']:
         urls1 = 'https://repo.continuum.io/pkgs/%s/%s/' % (channel, subdir),
-        print urls1
         index1 = fetch_index(urls1)
         urls2 = 'http://bremen/pkgs/%s/%s/' % (channel, subdir),
-        print urls2
         index2 = fetch_index(urls2)
 
         print channel, subdir, len(index1), len(index2)
