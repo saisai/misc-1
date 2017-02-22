5b1a08735d7a33e9a37c98f61c93cb527094e487
Ilan Schnell <ilanschnell@gmail.com>
2017-02-21 18:13:16 -0600

    add script

---
+++ repo/diff.py
@@ -0,0 +1,15 @@
+from libconda.fetch import fetch_index
+
+from repo.config import SUBDIRS
+
+
+for channel in 'free', 'pro':
+    for subdir in SUBDIRS + ['noarch']:
+        url1 = 'https://repo.continuum.io/pkgs/%s/%s/' % (channel, subdir)
+        print url1
+        index1 = fetch_index(tuple(url1,))
+        url2 = 'http://bremen/pkgs/%s/%s/' % (channel, subdir)
+        print url2
+        index2 = fetch_index(tuple(url2,))
+
+        print channel, subdir, len(index1), len(index2)
