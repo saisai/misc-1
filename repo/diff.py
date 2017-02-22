7a8b400aec0be15aa8b933700ba28be709d6f7d6
Ilan Schnell <ilanschnell@gmail.com>
2017-02-21 18:44:54 -0600

    fix tuples

--- repo/diff.py
+++ repo/diff.py
@@ -5,11 +5,11 @@ from repo.config import SUBDIRS
 
 for channel in 'free', 'pro':
     for subdir in list(SUBDIRS) + ['noarch']:
-        url1 = 'https://repo.continuum.io/pkgs/%s/%s/' % (channel, subdir)
-        print url1
-        index1 = fetch_index(tuple(url1,))
-        url2 = 'http://bremen/pkgs/%s/%s/' % (channel, subdir)
-        print url2
-        index2 = fetch_index(tuple(url2,))
+        urls1 = 'https://repo.continuum.io/pkgs/%s/%s/' % (channel, subdir),
+        print urls1
+        index1 = fetch_index(urls1)
+        urls2 = 'http://bremen/pkgs/%s/%s/' % (channel, subdir),
+        print urls2
+        index2 = fetch_index(urls2)
 
         print channel, subdir, len(index1), len(index2)
